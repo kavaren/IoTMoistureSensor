931eb2ff05a4984d410d422bc2365c3cc86740ae
kavaren <mateuszp2286@gmail.com>
2022-07-25 15:16:17 +0200

    Preparation for data logging

--- src/main.cpp
+++ src/main.cpp
@@ -15,37 +15,25 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 int i;
 void setup() {
   Serial.begin(9600);
-
   if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
     Serial.println(F("SSD1306 allocation failed"));
     for(;;);
   }
-  
+
+  display.setTextSize(2);
+  display.setTextColor(WHITE);  
 }
 
 void loop() {
 
   soilMoistureValue = analogRead(A0);  //put Sensor insert into soil
   int procent = map(soilMoistureValue, WaterValue, AirValue, 100, 0);
-  Serial.println(procent);
-
-  if(soilMoistureValue > WaterValue && soilMoistureValue < (WaterValue + intervals))
-  {
-    Serial.println("Very Wet");
-  }
-  else if(soilMoistureValue > (WaterValue + intervals) && soilMoistureValue < (AirValue - intervals))
-  {
-    Serial.println("Wet");
-  }
-  else if(soilMoistureValue < AirValue && soilMoistureValue > (AirValue - intervals))
-  {
-    Serial.println("Dry");
-  }
-  delay(100);
+  //Serial.print(millis());
+  //Serial.print(", ");
+  Serial.print(soilMoistureValue);
+  Serial.println();
 
   display.clearDisplay();
-  display.setTextSize(2);
-  display.setTextColor(WHITE);
   display.setCursor(0, 10);
   // Display static text
   display.println("Wilgotnosc");
@@ -66,7 +54,7 @@ void loop() {
   }
 
   display.display(); 
-  delay(2000);
+  delay(100);
   
 }
  
