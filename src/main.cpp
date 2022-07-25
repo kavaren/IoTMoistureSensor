271f41174a9ff54242b24e1e18a0435173546e28
kavaren <mateuszp2286@gmail.com>
2022-07-25 15:17:49 +0200

    Add analog values filtering

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,16 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include <Wire.h>
+#include <Ewma.h>
 
+Ewma adcFilter1(0.1);
+Ewma adcFilter2(0.01);    
+Ewma adcFilter3(0.001); 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 
-const int AirValue = 370;  
-const int WaterValue = 276; 
+const int AirValue = 361;  
+const int WaterValue = 275; 
 int intervals = (AirValue - WaterValue)/3;
 int soilMoistureValue = 0;
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
@@ -26,35 +30,41 @@ void setup() {
 
 void loop() {
 
+
+  int raw = analogRead(A0);
+  int filtered1 = adcFilter1.filter(raw);
+  int filtered2 = adcFilter2.filter(raw);
+  int filtered3 = adcFilter3.filter(raw);
+
   soilMoistureValue = analogRead(A0);  //put Sensor insert into soil
-  int procent = map(soilMoistureValue, WaterValue, AirValue, 100, 0);
-  //Serial.print(millis());
-  //Serial.print(", ");
+  int procent = map(filtered2, WaterValue, AirValue, 100, 0);
+  
   Serial.print(soilMoistureValue);
+  Serial.print(", ");
+  Serial.print(filtered1);
+  Serial.print(", ");
+  Serial.print(filtered2);
+  Serial.print(", ");
+  Serial.print(filtered3);
   Serial.println();
 
   display.clearDisplay();
   display.setCursor(0, 10);
-  // Display static text
   display.println("Wilgotnosc");
   display.print(procent);
   display.println("%");
 
-  if(soilMoistureValue > WaterValue && soilMoistureValue < (WaterValue + intervals))
+  if(procent <= 30){
+    display.print("Sucho");
+  }else if (procent > 30 && procent <= 70)
   {
-    display.println("MOKRO");
-  }
-  else if(soilMoistureValue > (WaterValue + intervals) && soilMoistureValue < (AirValue - intervals))
+    display.print("Wilgotno");
+  }else if (procent > 70)
   {
-    display.println("Wilgotno");
+    display.print("MOKRO");
   }
-  else if(soilMoistureValue < AirValue && soilMoistureValue > (AirValue - intervals))
-  {
-    display.println("Sucho");
-  }
-
-  display.display(); 
-  delay(100);
   
+  display.display(); 
+  delay(10);
 }
  
